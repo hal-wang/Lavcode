b54b002d5d2a2144b12642ed37c58f6ae7427754
hal-wang <hi@hal.wang>
2021-03-23 23:02:33 +0800

    0.2.4

--- Lavcode.Uwp/Global/Git.cs
+++ Lavcode.Uwp/Global/Git.cs
@@ -10,8 +10,6 @@
         public static int FeedbackIssueNumber { get; } = 1;
         public static int NoticeIssueNumber { get; } = 2;
         public static string GitUrl { get; } = "https://github.com";
-        public static string GitBaseApi { get; } = "https://api.github.com";
         public static string ReposUrl { get; } = $"{GitUrl}/{GitAccount}/{Repos}";
-        public static string AuthTitleKey { get; } = "lavcode_github_token";
     }
 }
