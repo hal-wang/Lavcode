56dce880b001e6ce32c34f640ca620cf48c41ba4
hal-wang <hi@hal.wang>
2021-12-07 21:19:59 +0800

    style: Comment list

--- Lavcode.Uwp/Controls/Comment/CommentList.xaml.cs
+++ Lavcode.Uwp/Controls/Comment/CommentList.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Octokit;
+﻿using Lavcode.Common;
+using Octokit;
+using System;
 using System.Collections.Generic;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -12,7 +15,6 @@ namespace Lavcode.Uwp.Controls.Comment
             this.InitializeComponent();
         }
 
-
         public IEnumerable<IssueComment> Comments
         {
             get { return (IEnumerable<IssueComment>)GetValue(CommentsProperty); }
@@ -22,5 +24,28 @@ namespace Lavcode.Uwp.Controls.Comment
         // Using a DependencyProperty as the backing store for Comments.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CommentsProperty =
             DependencyProperty.Register("Comments", typeof(IEnumerable<IssueComment>), typeof(CommentList), new PropertyMetadata(null));
+
+
+
+        public bool IsAuthorVisible
+        {
+            get { return (bool)GetValue(IsAuthorVisibleProperty); }
+            set { SetValue(IsAuthorVisibleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsAuthorVisible.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsAuthorVisibleProperty =
+            DependencyProperty.Register("IsAuthorVisible", typeof(bool), typeof(CommentList), new PropertyMetadata(true));
+
+        private async void MarkdownText_LinkClicked(object sender, Microsoft.Toolkit.Uwp.UI.Controls.LinkClickedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri(e.Link));
+        }
+
+        private async void AuthorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var comment = (sender as FrameworkElement).DataContext as IssueComment;
+            await Launcher.LaunchUriAsync(new Uri($"{RepositoryConstant.GitHubUrl}/{comment.User.Login}"));
+        }
     }
 }
