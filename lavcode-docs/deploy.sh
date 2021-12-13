b6d22aaec3ff59e3618a5f4dfa2d947001851d0a
hal-wang <hi@hal.wang>
2021-12-13 22:37:34 +0800

    docs: deploy

--- lavcode-docs/deploy.sh
+++ lavcode-docs/deploy.sh
@@ -2,5 +2,5 @@ cd .vuepress/dist/
 git init
 git add -A
 git commit -m deploy
-git remote add origin https://github.com/hal-wang/lavcode-docs.git
-git push origin master -f
+git remote add origin git@github.com:hal-wang/lavcode-docs.git
+git push origin main -f
