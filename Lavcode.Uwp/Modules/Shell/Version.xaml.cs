a7cae86b0f7754f95d1da346e4d370d880980259
hal-wang <hi@hal.wang>
2022-01-04 12:17:49 +0800

    feat: add provider title

--- Lavcode.Uwp/Modules/Shell/Version.xaml.cs
+++ Lavcode.Uwp/Modules/Shell/Version.xaml.cs
@@ -1,10 +1,31 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Lavcode.Model;
+using Lavcode.Uwp.Helpers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Lavcode.Uwp.Modules.Shell
 {
     public sealed partial class Version : UserControl
     {
-        public string VersionStr => Global.Version + " Beta";
+        public string VersionStr => Global.Version;
+
+        public Provider Provider => SettingHelper.Instance.Provider;
+
+
+
+
+        public Visibility ProviderVisibility
+        {
+            get { return (Visibility)GetValue(ProviderVisibilityProperty); }
+            set { SetValue(ProviderVisibilityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ProviderVisibility.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ProviderVisibilityProperty =
+            DependencyProperty.Register("ProviderVisibility", typeof(Visibility), typeof(Version), new PropertyMetadata(Visibility.Visible));
+
+
+
 
         public Version()
         {
