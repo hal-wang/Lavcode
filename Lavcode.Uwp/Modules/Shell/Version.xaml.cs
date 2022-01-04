41aabf458bfdc6b4feeda77db9dc899e9bcc7d42
hal-wang <hi@hal.wang>
2022-01-04 13:44:23 +0800

    fix: Hide provider when launching file

--- Lavcode.Uwp/Modules/Shell/Version.xaml.cs
+++ Lavcode.Uwp/Modules/Shell/Version.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Lavcode.Model;
 using Lavcode.Uwp.Helpers;
+using Lavcode.Uwp.Modules.SqliteSync;
+using Microsoft.Extensions.DependencyInjection;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -9,10 +11,9 @@ namespace Lavcode.Uwp.Modules.Shell
     {
         public string VersionStr => Global.Version;
 
-        public Provider Provider => SettingHelper.Instance.Provider;
-
-
-
+        private bool IsLaunchFile => ServiceProvider.Services.GetService<SqliteFileService>()?.OpenedFile != null;
+        private Provider Provider => SettingHelper.Instance.Provider;
+        private bool IsProviderVisible => ProviderVisibility == Visibility.Visible && !IsLaunchFile;
 
         public Visibility ProviderVisibility
         {
@@ -23,9 +24,6 @@ namespace Lavcode.Uwp.Modules.Shell
         // Using a DependencyProperty as the backing store for ProviderVisibility.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ProviderVisibilityProperty =
             DependencyProperty.Register("ProviderVisibility", typeof(Visibility), typeof(Version), new PropertyMetadata(Visibility.Visible));
-
-
-
 
         public Version()
         {
