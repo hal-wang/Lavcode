f4276a94b3dc253f199a98a1cfd60a434b95d613
hal-wang <hi@hal.wang>
2022-06-25 10:35:00 +0800

    chore(docs): remove cache files

--- lavcode-docs/docs/.vuepress/.temp/register-components/clientConfig.0210c1d4.js
+++
@@ -1,7 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export default {
-  enhance: ({ app }) => {    
-      app.component("Donate", defineAsyncComponent(() => import("C:/dev/h/Lavcode/lavcode-docs/docs/components/Donate.vue")))
-  },
-}
