a3440135fec71a40ffe5e606514e44f445598c3b
hal-wang <hi@hal.wang>
2022-09-10 22:19:46 +0800

    chore(node): rename secret

--- lavcode-node/src/services/cbapp.service.ts
+++ lavcode-node/src/services/cbapp.service.ts
@@ -3,8 +3,8 @@ import tcb = require("@cloudbase/node-sdk");
 export class CbappService {
   public readonly app = tcb.init({
     env: process.env.ENV_ID,
-    secretId: process.env.SECRET_ID,
-    secretKey: process.env.SECRET_KEY,
+    secretId: process.env.TENCENT_SECRET_ID,
+    secretKey: process.env.TENCENT_SECRET_KEY,
   });
   public readonly db = this.app.database();
 }
